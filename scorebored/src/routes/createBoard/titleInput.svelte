bd5a3c6a5f5d8ff1b74f818d84134f6234b529a8
GiangNPham <giangpn25@gmail.com>
2023-10-18 21:11:09 -0500

    Creating Board page

---
+++ scorebored/src/routes/createBoard/titleInput.svelte
@@ -0,0 +1,16 @@
+<script>
+    // @ts-nocheck
+    import "./titleInput.css";
+    let nameOfBoard = '';
+    </script>
+    
+    <div class="title-all">
+        <div class="title-header-container">
+            <h1 class="title-header-text">Name your board!</h1>
+        </div>
+    
+        <div class="title-box">
+            <input bind:value = {nameOfBoard} type="text" placeholder="Board's title" id="title-Input">
+    
+        </div>
+    </div>
---
+++ scorebored/src/routes/createBoard/typeInput.svelte
@@ -0,0 +1,22 @@
+<script>
+// @ts-nocheck
+
+    import "./typeInput.css";
+</script>
+
+<div class="type-all">
+    <div class="type-header-container">
+        <h1 class="type-header-text">Choose the type!</h1>
+    </div>
+
+    <div class="type-box">
+<!-- https://www.youtube.com/watch?v=sSF0bXFUFGM -->
+        <input type="radio" id="chart-box" name="radio-box" checked="checked"/>
+        <label for="chart-box" class="surround-box">Chart &ensp;&ensp;&nbsp;</label>
+
+        <input type="radio" id="checklist-box" name="radio-box"/>
+        <label for="checklist-box" class="surround-box">Checklist</label>
+
+
+    </div>
+</div>
