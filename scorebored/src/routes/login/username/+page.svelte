79511f005f99f2900c8a9c485037919fe9d6a9af
N0VA-code <ddoroki36@naver.com>
2023-11-04 19:20:49 -0500

    pre final

--- scorebored/src/routes/login/username/+page.svelte
+++ scorebored/src/routes/login/username/+page.svelte
@@ -1,52 +1,79 @@
 <script lang="ts">
-    import AuthCheck from '../../lib/components/AuthCheck.svelte';
-    import { db, user } from '../../lib/firebase';
-    import { doc, getDoc, writeBatch } from "firebase/firestore";
-  
-    let username = "";
-    let loading = false;
-    let isAvailable = false;
-    let debounceTimer: NodeJS.Timeout;
+  import AuthCheck from '../../lib/components/AuthCheck.svelte';
+  import { db } from '../../lib/firebase';
+  import { doc, getDoc, writeBatch } from "firebase/firestore";
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
+  import { onMount } from 'svelte';
 
-    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
-    $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
-    $: isTouched = username.length > 0;
-    $: isTaken = isValid && !isAvailable && !loading;
-  
-    async function checkAvailability() {
-      isAvailable = false;
-      clearTimeout(debounceTimer);
-      loading = true;
-      debounceTimer = setTimeout(async () => {
-          console.log("checking availability of", username);
-          const ref = doc(db, "usernames", username);
-          const exists = await getDoc(ref).then((doc) => doc.exists());
-          isAvailable = !exists;
-          loading = false;
-      }, 500);
+  let username = "";
+  let currentProgress = '';
+  let goalNeeded = '';
+  let isCompleted = false;
+  let labelX = '';
+  let labelY = '';
+  let loading = false;
+  let isAvailable = false;
+  let debounceTimer;
+  let currentUser = null;
+
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+  $: isTouched = username.length > 0;
+  $: isTaken = isValid && !isAvailable && !loading;
+
+  onMount(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        currentUser = user;
+      } else {
+        // User is signed out
+        currentUser = null;
+      }
+    });
+  });
+
+  async function checkAvailability() {
+    isAvailable = false;
+    clearTimeout(debounceTimer);
+    loading = true;
+    debounceTimer = setTimeout(async () => {
+      console.log("checking availability of", username);
+      const ref = doc(db, "usernames", username);
+      const docSnap = await getDoc(ref);
+      isAvailable = !docSnap.exists();
+      loading = false;
+    }, 500);
+  }
+
+  async function confirmUsername() {
+    console.log("confirming username", username);
+    if (!currentUser) {
+      console.error("User not found");
+      return;
     }
-  
-    async function confirmUsername() {
-      console.log("confirming username", username);
-      const batch = writeBatch(db);
-      batch.set(doc(db, "usernames", username), { uid: $user?.uid });
-      batch.set(doc(db, "users", $user!.uid), { 
-        username, 
-        photoURL: $user?.photoURL ?? null,
-        published: true,
-        bio: 'I am the Walrus',
-        links: [
-          {
-            title: 'Test Link',
-            url: 'https://kung.foo',
-            icon: 'custom'
-          }
-        ]
-      });
-      await batch.commit();
-      username = '';
-      isAvailable = false;
-    }
+    const uid = currentUser.uid; // Use the current user's UID
+
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: uid });
+    batch.set(doc(db, "users", uid), { 
+      username, 
+      currentProgress, 
+      goalNeeded, 
+      isCompleted, 
+      labels: { x: labelX, y: labelY }
+    });
+    await batch.commit();
+    // Clear the form
+    username = '';
+    currentProgress = '';
+    goalNeeded = '';
+    isCompleted = false;
+    labelX = '';
+    labelY = '';
+    isAvailable = false;
+  }
 </script>
 
 <AuthCheck>
@@ -61,23 +88,40 @@
       class:input-warning={isTaken}
       class:input-success={isAvailable && isValid && !loading}
     />
-    <div class="my-4 min-h-16 px-8 w-full">
-      {#if loading}
-        <p class="text-secondary">Checking availability of @{username}...</p>
-      {/if}
-      {#if !isValid && isTouched}
-        <p class="text-error text-sm">
-          must be 3-16 characters long, alphanumeric only
-        </p>
-      {/if}
-      {#if isValid && !isAvailable && !loading}
-        <p class="text-warning text-sm">
-          @{username} is not available
-        </p>
-      {/if}
-      {#if isAvailable}
-        <button class="btn btn-success">Confirm username @{username} </button>
-      {/if}
-    </div>
+    <input
+      type="text"
+      placeholder="Current Progress"
+      class="input w-full my-2"
+      bind:value={currentProgress}
+    />
+    <input
+      type="text"
+      placeholder="Goal Needed"
+      class="input w-full my-2"
+      bind:value={goalNeeded}
+    />
+    <label class="checkbox-label">
+      <input
+        type="checkbox"
+        class="checkbox"
+        bind:checked={isCompleted}
+      />
+      Is Completed?
+    </label>
+    <input
+      type="text"
+      placeholder="Label X"
+      class="input w-full my-2"
+      bind:value={labelX}
+    />
+    <input
+      type="text"
+      placeholder="Label Y"
+      class="input w-full my-2"
+      bind:value={labelY}
+    />
+    <button class="btn btn-success" type="submit" disabled={!isValid || !isAvailable || loading}>
+      Confirm Username
+    </button>
   </form>
 </AuthCheck>
